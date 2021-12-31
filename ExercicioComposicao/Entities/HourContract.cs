3c05e2c22e43e499481ab0c8de892fada95bc86e
Adriel Arruda <adriel.marcos.arruda@gmail.com>
2021-12-31 14:23:50 -0300

    Class HourContract implemented

---
+++ ExercicioComposicao/Entities/HourContract.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioComposicao.Entities
+{
+    internal class HourContract
+    {
+        public DateTime date { get; set; }
+        public double valuePerHour { get; set; }
+        public double hours { get; set; }
+
+        public HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valuePerHour, double hours)
+        {
+            this.date = date;
+            this.valuePerHour = valuePerHour;
+            this.hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return hours * valuePerHour;
+        }
+    }
+}
